4bfb8ac5b00515725d12d5293764539f9f91d16b
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-08-19 10:34:44 -0400

    added urls

--- RESTful/service/urls.py
+++ RESTful/service/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
 urlpatterns = [
-    
+    path('songs/', views.SongsList.as_view()),
+    path('snippets/<int:pk>/', views.SongsDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
