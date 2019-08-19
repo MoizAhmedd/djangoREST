a511fae1438d45a0e1129a0465a6c39d48900b95
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-08-19 09:41:24 -0400

    create model

--- RESTful/service/models.py
+++ RESTful/service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Songs(models.Model):
+    song = models.TextField()
+    artist = models.TextField()
+    def __str__(self):
+        return "{} by {}".format(self.song,self.artist) 
